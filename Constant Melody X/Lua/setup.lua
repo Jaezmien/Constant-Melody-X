d902867a1e695156f9b5906c9beae3b79afc2efb
Jaezmien Naejara <bootalpha@gmail.com>
2020-03-15 08:36:57 +0800

    oops spelling

--- Constant Melody X/Lua/setup.lua
+++ Constant Melody X/Lua/setup.lua
@@ -42,8 +42,8 @@ end
 
 -- wee hee
 if not CONSTMELODY.MinimumVersion('V4') then
-    local disp_width = PREFSMAN:GetPrefence('DisplayWidth')
-    local disp_height = PREFSMAN:GetPrefence('DisplayHeight')
+    local disp_width = PREFSMAN:GetPreference('DisplayWidth')
+    local disp_height = PREFSMAN:GetPreference('DisplayHeight')
     --
     SCREEN_WIDTH = 480 * (disp_width / disp_height)
     SCREEN_CENTER_X = SCREEN_WIDTH / 2
